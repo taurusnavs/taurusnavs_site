be1679dcd475d0c42ab35df0116b4f1cf184c140
Haga <taurus.navs16@gmail.com>
2022-06-02 13:39:29 +0300

    Add files via upload

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,1 +1,23 @@
+$(function() {
+  //hide about-me home
+    $("#navbar #s1").one('click', function() {
+        $("#home").show();
+      });
+    $("#navbar #h1").one('click', function() {
+        $("#home").hide();
+      });
 
+      $('.scrolltotop i').hide();
+
+      $(window).scroll(function() {
+        
+        if ($(this).scrollTop()>8)
+         {
+            $('.scrolltotop i').fadeIn(500);
+         }
+        else
+         {
+          $('.scrolltotop i').hide(500);
+         }
+     });
+});
